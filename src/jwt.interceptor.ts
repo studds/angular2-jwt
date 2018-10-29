b830a15cb7099b7c184bea0195f84d1a3f2c7f17
studds <daniel.studds@gmail.com>
2018-10-30 09:52:57 +1100

    #576: Check whitelist / blacklist before retrieving token

--- src/jwt.interceptor.ts
+++ src/jwt.interceptor.ts
@@ -104,14 +104,21 @@ export class JwtInterceptor implements HttpInterceptor {
     request: HttpRequest<any>,
     next: HttpHandler
   ): Observable<HttpEvent<any>> {
+    if (
+      !this.isWhitelistedDomain(request) ||
+      this.isBlacklistedRoute(request)
+    ) {
+      return next.handle(request);
+    }
+
     const token = this.tokenGetter();
 
     if (token instanceof Promise) {
-      return from(token).pipe(mergeMap(
-        (asyncToken: string | null) => {
+      return from(token).pipe(
+        mergeMap((asyncToken: string | null) => {
           return this.handleInterception(asyncToken, request, next);
-        }
-      ));
+        })
+      );
     } else {
       return this.handleInterception(token, request, next);
     }
